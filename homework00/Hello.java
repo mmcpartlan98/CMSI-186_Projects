d84a8d846cea7f43b1921b16293dfb21b1296dbe
Matt McPartlan <mmcpartlan98@gmail.com>
2018-01-16 01:57:45 -0800

    Updated template for personal use.
    CMSI 186 Homework 00
    Submission 1

---
+++ homework00/Hello.java
@@ -0,0 +1,23 @@
+
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  Hello.java
+ *  Purpose       :  Prints 'Hello, world!' to the console.
+ *  Author        :  Matt McPartlan
+ *  Date          :  01/15/2018
+ *  Description   :  First CMSI 186 assignment.
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  Exceptions    :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision History
+ *  -----------------
+ *   Rev      Date     Modified by:    Reason for change/modification
+ *  -----  ----------  ------------    --------------------------------------------------------------------
+ *  1.0.0  2017-01-02  B.J. Johnson    Initial writing and release
+ *  1.1.0  01/15/2018  Matt McPartlan  Filling out template/getting set up
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+public class Hello {
+   public static void main( String args[] ) {
+      System.out.println( "Hello, world!" );
+   }
+}
