23837bb2732a10e1fa8053f3a4c138dbf90094b2
Matt McPartlan <mmcpartlan98@gmail.com>
2018-04-23 01:10:12 -0700

    New Tests and updated documentation

--- homework06/Fibonacci.java
+++ homework06/Fibonacci.java
@@ -1,21 +1,17 @@
 /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-*  File name     :  ClockSolver.java
-*  Purpose       :  The main program for the ClockSolver class
+*  File name     :  Fibonacci.java
+*  Purpose       :  The main program for the Fibonacci class
 *  @see
-*  @author       :  B.J. Johnson
-*  Date written  :  2017-02-28
-*  Description   :  This class provides a bunch of methods which may be useful for the ClockSolver class
-*                   for Homework 4, part 1.  Includes the following:
-*
-*  Notes         :  None right now.  I'll add some as they occur.
-*  Warnings      :  None
-*  Exceptions    :  IllegalArgumentException when the input arguments are "hinky"
+*  @author       :  Matt McPartlan
+*  Date written  :  2018-04-23
+*  Description   :  This class prints out the Fibonacci sequence using the BrobInt class
+*  Exceptions    :  IllegalArgumentException when the input arguments are noninteger values
 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 *  Revision Histor
 *  ---------------
-*            Rev      Date     Modified by:  Reason for change/modification
-*           -----  ----------  ------------  -----------------------------------------------------------
-*  @version 1.0.0  2017-02-28  B.J. Johnson  Initial writing and release
+*            Rev      Date     Modified by:    Reason for change/modification
+*           -----  ----------  --------------  -----------------------------------------------------------
+*  @version 1.0.0  2018-04-23  Matt McPartlan  Initial writing and release
 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 public class Fibonacci {
@@ -39,15 +35,7 @@ public class Fibonacci {
     }
     return Integer.parseInt(arg);
   }
-
-  /**
-  *  The main program starts here
-  *  remember the constraints from the project description
-  *  @see  http://bjohnson.lmu.build/cmsi186web/homework04.html
-  *  @param  args  String array of the arguments from the command line
-  *                args[0] is the angle for which we are looking
-  *                args[1] is the time slice; this is optional and defaults to 60 seconds
-  */
+  // Accepts one argument: Integer n -  the number of Fibbonacci numbers to display.
   public static void main( String args[] ) {
     BrobInt fibonacci = new BrobInt("0");
     BrobInt nextFib = new BrobInt("1");
